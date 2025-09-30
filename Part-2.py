5255d091e81cc7e93d8612840aea0a66d9684851
Mohamad Sabeh <mohamadsabeh1234@gmail.com>
2025-09-30 13:15:23 +0300

    Add valid item to cart with quantity and update total aswell as show error for invalid items

--- Part-2.py
+++ Part-2.py
@@ -14,3 +14,24 @@ while item != "done":
     for i in Groceries:
         if item == i:
             found = 1
+            
+    if found == 1:
+        Q = input("choose how many: ").strip()
+        if Q.isdigit():
+            quantity = int(Q)
+        else:
+            print("Invalid quantity, using 1 instead.")
+            quantity = 1
+
+        if item in cart:
+            cart[item] += quantity
+        else:
+            cart[item] = quantity
+
+        total += Groceries[item] * quantity
+    else:
+        print("Invalid Item.")
+
+    item = input("What do you want to buy? ").strip().lower()
+
+print("You bought: ", cart)
